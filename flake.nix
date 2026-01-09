b3257a4815ef6d60ef938e21430f1d59fb6d0674
michael <michael@mabbott.dev>
2026-01-09 23:43:27 +0000

    chore: remove pointless commands

--- flake.nix
+++ flake.nix
@@ -4,11 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    axon = {
-      url = "path:../axon";
-      inputs.nixpkgs.follows = "nixpkgs";
-    };
   };
 
   outputs = { self, nixpkgs, flake-utils, axon }:
@@ -46,29 +41,8 @@
 
           shellHook = ''
             echo "Cortex - Synapse Platform Control Plane"
-            echo ""
-            echo "Commands:"
-            echo "  just fleet-up       Create Kind clusters"
-            echo "  just fleet-down     Destroy clusters"
-            echo "  just fleet-status   Check health"
-            echo "  just bootstrap      Bootstrap Flux"
-            echo "  just validate       Validate manifests"
-            echo ""
           '';
         };
-
-        checks.default = pkgs.runCommand "validate-cortex" {
-          buildInputs = [ pkgs.kubeconform pkgs.kustomize ];
-        } ''
-          cd ${self}
-          for dir in deploy/*/; do
-            if [ -f "$dir/kustomization.yaml" ]; then
-              echo "Validating $dir..."
-              kustomize build "$dir" | kubeconform -strict -skip CustomResourceDefinition
-            fi
-          done
-          touch $out
-        '';
       }
     );
 }
