b768a99a0011e1a5529dec4620a42fbb7ae80144
michael <michael@mabbott.dev>
2026-01-18 21:20:16 +0000

    refactor: extract composer package from xtenant

--- apis/management/functions/xtenant/fn.go
+++ apis/management/functions/xtenant/fn.go
@@ -3,6 +3,7 @@ package main
 import (
 	"context"
 
+	"github.com/mdaops/cortex/configurations/pkg/composer"
 	"github.com/mdaops/cortex/configurations/pkg/resources"
 	rbacv1 "k8s.io/api/rbac/v1"
 
@@ -15,75 +16,41 @@ import (
 	"github.com/crossplane/function-sdk-go/response"
 )
 
-// Function implements the Crossplane composition function for Tenant resources.
 type Function struct {
 	fnv1.UnimplementedFunctionRunnerServiceServer
-
 	log logging.Logger
 }
 
-// composer holds state for composing resources from an XR.
-type composer struct {
-	oxr     *resource.Composite
-	desired map[resource.Name]any
-}
-
-func newComposer(oxr *resource.Composite) *composer {
-	return &composer{
-		oxr:     oxr,
-		desired: make(map[resource.Name]any),
-	}
-}
-
-func (c *composer) getString(path string) string {
-	val, _ := c.oxr.Resource.GetString(path)
-	return val
-}
-
-func (c *composer) getStringArray(path string) []string {
-	val, _ := c.oxr.Resource.GetStringArray(path)
-	return val
-}
-
-func (c *composer) featureEnabled(feature string) bool {
-	enabled, err := c.oxr.Resource.GetBool("spec.features." + feature + ".enabled")
-	return err == nil && enabled
-}
-
-func (c *composer) add(name resource.Name, obj any) {
-	c.desired[name] = obj
-}
-
-func (c *composer) name() string {
-	return c.getString("spec.name")
-}
-
-func (c *composer) composeBase() error {
-	name := c.name()
-	if name == "" {
-		return errors.New("spec.name is required")
+func composeBase(c *composer.Composer) error {
+	name := c.GetString("spec.name")
+	if err := c.Err(); err != nil {
+		return err
 	}
 
-	c.add("namespace", resources.NewTenantNamespace(name))
+	c.Add("namespace", resources.NewTenantNamespace(name))
 
-	project, err := resources.NewTenantProject(name, c.getString("spec.description"), c.getStringArray("spec.sourceRepos"))
+	project, err := resources.NewTenantProject(name, c.GetString("spec.description"), c.GetStringArray("spec.sourceRepos"))
+	c.ClearErrs()
 	if err != nil {
 		return errors.Wrap(err, "spec.sourceRepos is required")
 	}
-	c.add("project", project)
+	c.Add("project", project)
 	return nil
 }
 
-func (c *composer) composeArgoWorkflows() {
-	if !c.featureEnabled("argoWorkflows") {
+func composeArgoWorkflows(c *composer.Composer) {
+	if !c.GetBool("spec.features.argoWorkflows.enabled") {
+		c.ClearErrs()
 		return
 	}
-	name := c.name()
-	c.add("argo-workflow-sa", resources.NewServiceAccount(resources.ServiceAccountConfig{
+	name := c.GetString("spec.name")
+	c.ClearErrs()
+
+	c.Add("argo-workflow-sa", resources.NewServiceAccount(resources.ServiceAccountConfig{
 		Name:      "argo-workflow",
 		Namespace: name,
 	}))
-	c.add("argo-workflow-role", resources.NewRole(resources.RoleConfig{
+	c.Add("argo-workflow-role", resources.NewRole(resources.RoleConfig{
 		Name:      "argo-workflow",
 		Namespace: name,
 		Rules: []rbacv1.PolicyRule{{
@@ -92,7 +59,7 @@ func (c *composer) composeArgoWorkflows() {
 			Verbs:     []string{"create", "patch"},
 		}},
 	}))
-	c.add("argo-workflow-rolebinding", resources.NewRoleBinding(resources.RoleBindingConfig{
+	c.Add("argo-workflow-rolebinding", resources.NewRoleBinding(resources.RoleBindingConfig{
 		Name:               "argo-workflow",
 		Namespace:          name,
 		RoleName:           "argo-workflow",
@@ -100,7 +67,6 @@ func (c *composer) composeArgoWorkflows() {
 	}))
 }
 
-// RunFunction composes namespace and ArgoCD project resources for a Tenant.
 func (f *Function) RunFunction(_ context.Context, req *fnv1.RunFunctionRequest) (*fnv1.RunFunctionResponse, error) {
 	rsp := response.To(req, response.DefaultTTL)
 
@@ -115,14 +81,14 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1.RunFunctionRequest) 
 		"xr-kind", oxr.Resource.GetKind(),
 	)
 
-	c := newComposer(oxr)
+	c := composer.New(oxr)
 
-	if err := c.composeBase(); err != nil {
+	if err := composeBase(c); err != nil {
 		response.Fatal(rsp, err)
 		return rsp, nil
 	}
 
-	c.composeArgoWorkflows()
+	composeArgoWorkflows(c)
 
 	desired, err := request.GetDesiredComposedResources(req)
 	if err != nil {
@@ -130,7 +96,7 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1.RunFunctionRequest) 
 		return rsp, nil
 	}
 
-	for name, obj := range c.desired {
+	for name, obj := range c.Desired() {
 		res := composed.New()
 		if err := resources.ConvertViaJSON(res, obj); err != nil {
 			response.Fatal(rsp, errors.Wrapf(err, "cannot convert %s to unstructured", name))
@@ -144,7 +110,7 @@ func (f *Function) RunFunction(_ context.Context, req *fnv1.RunFunctionRequest) 
 		return rsp, nil
 	}
 
-	log.Info("Composed tenant resources", "tenant", c.name())
+	log.Info("Composed tenant resources", "tenant", c.GetString("spec.name"))
 	response.ConditionTrue(rsp, "FunctionSuccess", "Success").TargetCompositeAndClaim()
 
 	return rsp, nil
